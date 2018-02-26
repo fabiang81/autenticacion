b6a0b0a735c5502598b572552194d8b0add94fe9
mercedes-rs <mramirez@novasolutionsystems.com>
2018-02-26 16:45:44 -0600

    Se agrega cambio formato Fecha ultimo acceso

---
+++ src/main/java/mx/com/beo/util/Constantes.java
@@ -0,0 +1,11 @@
+package mx.com.beo.util;
+
+public class Constantes {
+	
+public static final String FORMATO_FECHA = "yyyyMMddHHmmss'.0Z'";
+public static final String FORMATO_FECHA_SERVICIO = "yyyy-MM-dd'T'HH:mm:ss";
+
+public static final String ERROR_FORMATO_FECHA_SERVICIO = "Error en Formato fecha Ultimo Acceso";
+public static final String ERROR_FORMATO_FECHA = "Error en Formato fecha";
+
+}
---
+++ src/main/java/mx/com/beo/util/Utilerias.java
@@ -0,0 +1,56 @@
+package mx.com.beo.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Utilerias {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Utilerias.class);
+
+	/**
+	 * Establece un formato de fecha.
+	 * 
+	 * @param fecha a la que se desea dar formato.
+	 * @return fecha con formato establecido dd-MM-yyyy HH:mm:ss
+	 */
+	public static String formatoFecha(Date fecha, String formatoFecha) {
+		
+		SimpleDateFormat formato = new SimpleDateFormat(formatoFecha);
+		String fechaFormato = null;
+		
+		try {
+			fechaFormato = formato.format(fecha);
+		} catch (Exception e) {
+			LOGGER.error(Constantes.ERROR_FORMATO_FECHA, e);
+		}
+		return fechaFormato;
+	}
+	
+	
+	/**
+	 * Establece un formato de fecha.
+	 * 
+	 * @param fecha a la que se desea dar formato .
+	 * @return fecha con formato establecido yyyy-MM-dd'T'HH:mm:ss
+	 */
+	public static String fechaFormatoServicio(String fecha, String formatoFecha) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat(formatoFecha);
+		String fechaFormato = null;
+		Date date = null;
+		
+		try {
+			date = sdf.parse(fecha);
+			fechaFormato = formatoFecha(date, Constantes.FORMATO_FECHA_SERVICIO);
+		} catch (Exception e) {
+			LOGGER.error(Constantes.ERROR_FORMATO_FECHA_SERVICIO, e);
+		}
+
+		return fechaFormato;
+	}
+	
+}
