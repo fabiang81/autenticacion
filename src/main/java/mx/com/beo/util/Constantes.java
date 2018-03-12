54b7190dc9ec57998f852873da6eaefa3db0a3bd
mercedes-rs <mramirez@novasolutionsystems.com>
2018-03-12 08:33:57 -0600

    Se agregan cambios por calificacion sonar

--- src/main/java/mx/com/beo/util/Constantes.java
+++ src/main/java/mx/com/beo/util/Constantes.java
@@ -10,11 +10,45 @@ public class Constantes {
 
 	public static final String ERROR_FORMATO_FECHA_SERVICIO = "Error en Formato fecha Ultimo Acceso";
 	public static final String ERROR_FORMATO_FECHA = "Error en Formato fecha";
+	
+    public static final String  EXCEPTION_HEADERS = "Valida Headers exception";
 
 	public static final String  PROTOCOLO = "PROTOCOLO";
 	public static final String  HOSTNAME_BEO = "HOSTNAME_BEO";
 	public static final String  PUERTO = "PUERTO";
 	public static final String  BASEPATH = "BASEPATH";
 	public static final String  BITACORA_URL = "BITACORA_URL";
+	public static final String  URL_MODIFICA_CONTRATO = "URL_MODIFICA_CONTRATO";
+	
+	public static final String ENDPOINT = "endpoint";
+	public static final String METHOD = "method";
+	public static final String CONNECT_TIMEOUT = "connectTimeout";
+	public static final String READ_TIMEOUT = "readTimeout";
+	public static final String CONNECT_REQUEST_TIMEOUT = "connectionRequestTimeout";
+	public static final String HEADER = "headers";
+	public static final String BODY = "body";
+	public static final String BANDERA_ACCESO = "banderaAcceso";
+	public static final String CLIENTE ="cliente";
+	public static final String ENVIO_NOTIFICACION = "envioNotificacion";
+
+	public static final String RESPONSE_STATUS = "responseStatus";
+	public static final String RESPONSE_ERROR = "responseError";
+	
+	public static final String LOG_ENDPOINT_CAMBIO_CONTRASENA = "EndPoint cambioContrasena";
+	public static final String LOG_ENDPOINT_CONTRASENA = "EndPoint Contrasena"; 
+	public static final String LOG_CONTRASENAS_MODIFICAR = "Las contraseñas a modificar son diferentes";
+	public static final String LOG_CONTRATO_ACEPTADO = "el contrato ya esta aceptado";
+	public static final String LOG_CONTRATO_NO_ACEPTADO = "el contrato no esta aceptado";
+	public static final String LOG_OK_BANDERA_ACCESO = "Ok, banderaAcceso trae 1";
+	public static final String LOG_OK_CONTRATO_MODIFICADO = "Ok, Se ha modificado el contrato";
+	public static final String LOG_OK_MOSTRAR_CONTRATO = "OK, Se envia mostrar contrato";
+	public static final String LOG_ENDPOINT_ACCESO_CLIENTES = "EndPoint accesoCliente";
+	public static final String LOG_OK_BANDERA_ACCESO_DATO_DIFERENTE = "OK, banderaAcceso otro dato diferente";
+
+	public static final String LOG_OK = "OK";
+	
+	//Tipo de respuesta del servicio
+	public static final String  VACIO = "";
+
 
 }
